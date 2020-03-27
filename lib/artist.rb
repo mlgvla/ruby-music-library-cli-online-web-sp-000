c9b6e5969c7c40657f8d98669b51cff2a9386240
Monica Gerard <mlgerardvla@gmail.com>
2020-03-27 22:51:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
     save
   end
 
@@ -23,6 +24,10 @@ class Artist
   def self.create(name)
     artist = Artist.new(name)
     artist
+  end
+
+  def songs
+    @songs
   end
 
 end
