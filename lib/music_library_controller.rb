434367c26bb46de17616ad1acb222a5faedaa50c
Monica Gerard <mlgerardvla@gmail.com>
2020-03-28 17:44:59 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,18 +23,57 @@ class MusicLibraryController
     input = gets.chomp
 
     case input
-      # when "list songs"
-      # when "list artists"
-      # when "list genres"
-      # when "list artist"
-      # when "list genre"
-      # when "play song"
+      when "list songs"
+      when "list artists"
+      when "list genres"
+      when "list artist"
+      when "list genre"
+      when "play song"
       when "exit"
         exit
       else
         call
-      end
+    end
+  end
+
+  # MusicLibraryController - CLI Methods
+
+  def list_songs
+    sorted_library = Song.all.sort_by  {|song| song.name}
+    counter = 1
+    sorted_library.each do |song|
+      puts"#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      counter += 1
+    end
+  end
+
+  def list_artists
+    sorted_library = Artist.all.sort_by  {|artist| artist.name}
+    counter = 1
+    sorted_library.each do |artist|
+      puts"#{counter}. #{artist.name}"
+      counter += 1
+    end
+  end
+
+  def list_genres
+    sorted_library = Genre.all.sort_by  {|genre| genre.name}
+    counter = 1
+    sorted_library.each do |genre|
+      puts"#{counter}. #{genre.name}"
+      counter += 1
+    end
+  end
+
+  def list_songs_by_artist
 
   end
 
+  def list_songs_by_genre
+
+  end
+
+  def play_song
+
+  end
 end
